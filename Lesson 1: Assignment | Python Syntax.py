640d1cb36d0910538c9d71053fb48ff81cc60c94
ShiqiWangTony1 <shiqi.wang0728@gmail.com>
2024-05-29 21:15:11 -0400

    Assignment1 Python Syntax

---
+++ Lesson 1: Assignment | Python Syntax.py
@@ -0,0 +1,72 @@
+# Task 1: Code Correction
+# weather = "sunny"
+# if weather == "sunny":
+#     print("Wear sunglasses!")
+# else:
+#     print("Take an umbrella")
+
+
+# Task 2: Your Mood Today
+# mood = input("How do you feel today?: ")
+
+# if mood.lower() == "happy":
+#     print("That's great to hear!")
+# elif mood.lower() == "sad":
+#     print("I hope your day gets better!")
+
+
+# Task 1: Code Correction
+# Pi_value = 3.14
+# userAge = 25
+# user_location = "New York"
+# MAXLIMIT = 1000
+
+# PI = 3.14   # Because for constants, we can use all upper case
+# user_age = 25   # use snake_case for variable names
+# user_location = "New York"  # Good
+# MAX_LIMIT = 1000 # Because for constants, we can use all upper case
+
+
+# Task 1: Code Correction
+# variable_a = "Hello, World!"
+# variable_b = 23
+# variable_c = 3.14
+# variable_d = True
+
+# print(f"variable_a is {type(variable_a)}")
+# print(f"variable_b is {type(variable_b)}")
+# print(f"variable_c is {type(variable_c)}")
+# print(f"variable_d is {type(variable_d)}")
+
+
+# Task 1: Grocery Store Math
+# water = 1.5
+# bread = 3
+# beef = 9.75
+
+# total_price = water + bread + beef
+# print(f"Total price of water, bread, and beef is ${total_price}")
+
+# Task 2: Bank Interest (Extra Credit)
+# initial_amount = 10000
+# interest_rate = 0.07
+# total_amount = initial_amount * (1+interest_rate)
+# print(f"The total amount is ${total_amount}")
+
+
+# Task 1: Validating Calculations
+# calculate_normal = 5 * 4 + 3 - 2 * 8
+# calculate_parentheses = (5 * 4) + 3 - (2 * 8)
+# print(f"Result of normal calculation: {calculate_normal}")
+# print(f"Result of calculation with parentheses: {calculate_parentheses}")
+
+# if calculate_normal == calculate_parentheses:
+#     print("The results match.")
+# else:
+#     print("The results don't match.")
+
+
+# Task 2: Mix and Match
+# (9 + 9 > 10) or (3 * 3 == 9)
+# result = (9 + 9 > 10) or (3 * 3 == 9)
+# print(f"The result is {result}")
